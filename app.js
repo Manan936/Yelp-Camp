95d36d69148fc766d6004f8fb6b6a27a2ae322e8
Manan Upadhyay <manan.upa911@gmail.com>
2023-05-12 09:00:22 +0530

    Added Files

---
+++ app.js
@@ -0,0 +1,122 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+// console.log(process.env.SECRET);
+// console.log(process.env.API_KEY);
+
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
+const session = require("express-session");
+const flash = require("connect-flash");
+const ExpressError = require("./utils/ExpressError");
+const methodOverride = require("method-override");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
+const helmet = require("helmet");
+// const dbUrl = process.env.DbUrl
+
+const dbUrl = "mongodb://127.0.0.1:27017/yelp-camp";
+const mongoSanitize = require("express-mongo-sanitize");
+
+const userRoutes = require("./routes/users");
+const campgroundRoutes = require("./routes/campground");
+const reviewRoutes = require("./routes/review");
+
+const MongoDBStore = require("connect-mongo");
+
+mongoose
+  .connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MONGO CONNECTION OPEN!!!");
+  })
+  .catch((err) => {
+    console.log("OH MONGO ERROR!!!!");
+    console.log(err);
+  });
+
+const app = express();
+
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  mongoSanitize({
+    replaceWith: "_",
+  })
+);
+
+const secret = "thisshouldbeabettersecret!";
+// or process.env.SECRET
+
+const store = new MongoDBStore({
+  mongoUrl: dbUrl,
+  secret,
+  touchAfter: 24 * 60 * 60,
+});
+
+store.on("error", function (e) {
+  console.log("session error", e);
+});
+
+const sessionConfig = {
+  store,
+  name: "session",
+  secret,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    // secure: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+
+app.use(session(sessionConfig));
+app.use(flash());
+// app.use(helmet());
+
+app.use(passport.initialize()); // this is to initialize
+app.use(passport.session()); // this is for the middleware this should be after session
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser()); // store in session
+passport.deserializeUser(User.deserializeUser()); // unstore in session
+
+app.use((req, res, next) => {
+  // console.log(req.session);
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
+});
+
+app.use("/", userRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/reviews", reviewRoutes);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
+});
+app.use((err, req, res, next) => {
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = "Something went wrong";
+  res.status(statusCode).render("error", { err });
+});
+app.listen(3000, () => {
+  console.log("Serving on port 3000");
+});
